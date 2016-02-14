27a9fe0e1b51819a459df337ff8b45c065328cbe
qiyidk <qiyidk@gmail.com>
2016-02-14 13:12:52 -0500

    fix bugs

--- njit.cs602.qiyi.assignment1/src/njit/cs602/qiyi/assignment1/group/Quiz.java
+++ njit.cs602.qiyi.assignment1/src/njit/cs602/qiyi/assignment1/group/Quiz.java
@@ -12,17 +12,19 @@ import java.util.Scanner;
  * @version 2016-2-11
  */
 public class Quiz {    
-    public static final String[] questions = new String[]{};
-    public static final String[] choices = new String[]{};
-    public static final int[] answers = new int[]{};
+    public static final String[] questions = new String[]{"1","2","3","4","5"};
+    public static final String[] choices = new String[]{"1,2,3,4","1,2,3,4","1,2,3,4","1,2,3,4","1,2,3,4"};
+    public static final int[] answers = new int[]{1, 2, 3, 4, 4};
     public static void doQuiz(){
         System.out.println("Global Warming Facts Quiz");
+        System.out.println("You will have five multiple choice questions");
+        System.out.println("Your answer should be a number. For example, 123 represents choose 1,2,3");
         Scanner in = new Scanner(System.in);
         int correct = 0;
-        for (int i = 1; i <= 5; i++){
-            System.out.println(i + ":" + questions[i - 1]);
-            System.out.println(i + ":" + choices[i - 1]);
-            System.out.print("Your answer :");
+        for (int i = 0; i <= 4; i++){
+            System.out.println(i + 1 + ":" + questions[i]);
+            System.out.println(choices[i]);
+            System.out.print("Your answer: ");
             try{
                 int answer = in.nextInt();
                 if (answer == answers[i]) correct++;
@@ -35,10 +37,11 @@ public class Quiz {
                 // do nothing      
             }
         }
+        System.out.println("Your score : " + correct);
         if (correct == 5) System.out.println("Excellent!");
         else if (correct == 4) System.out.println("Very Good!");
         else System.out.println("Time to brush up on your knowledge of global warming");
-        System.out.println("Questions come from XX");
+        System.out.println("You can find some useful information of global warming on XXX");
         in.close();
     }
     

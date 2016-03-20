9a36d2db1ad5ef9632ade92b3565fe9beb9182a0
qiyidk <qiyidk@gmail.com>
2016-03-20 17:13:16 -0400

    create

---
+++ njit.cs602.qiyi.assignment2/src/njit/cs602/qiyi/assignment2/group/payroll/PieceWorker.java
@@ -0,0 +1,58 @@
+package njit.cs602.qiyi.assignment2.group.payroll;
+
+/**
+ * <p>
+ * PieceWorker
+ * </p>
+ *
+ * @author qiyi
+ * @version 2016Äê3ÔÂ20ÈÕ
+ */
+public class PieceWorker extends Employee {
+    private double wage; // wage per piece
+    private double pieces; // number of pieces produced
+
+    public PieceWorker(String firstName, String lastName,
+            String socialSecurityNumber, double wage, double pieces) {
+        super(firstName, lastName, socialSecurityNumber);
+        // TODO Auto-generated constructor stub
+        
+        if (wage < 0.0) // validate wage
+            throw new IllegalArgumentException(
+               "wage per piece must be >= 0.0");
+        this.wage = wage;
+        this.pieces = pieces;
+    }
+    
+    public double getWage() {
+        return wage;
+    }
+
+    public void setWage(double wage) {
+        if (wage < 0.0) // validate wage
+            throw new IllegalArgumentException(
+               "wage per piece must be >= 0.0");
+        this.wage = wage;
+    }
+
+    public double getPieces() {
+        return pieces;
+    }
+
+    public void setPieces(double pieces) {
+        this.pieces = pieces;
+    }
+
+
+    @Override
+    public double earnings() {
+        // TODO Auto-generated method stub
+        return wage * pieces;
+    }
+    @Override
+    public String toString(){
+        return String.format("piece worker: %s%n%s: $%,.2f; %s: %,.2f",
+                super.toString(), "wage per piece", getWage(),                     
+                "pieces produced", getPieces());         
+    }
+}

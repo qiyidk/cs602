45da729b4a79eda74f059f60ec1a7a1ee245372d
qiyidk <qiyidk@gmail.com>
2016-04-17 18:41:13 -0400

    update

--- njit.cs602.qiyi.assignment3/src/njit/cs602/qiyi/assignment3/genericMethod/InvalidSubscriptException.java
+++ njit.cs602.qiyi.assignment3/src/njit/cs602/qiyi/assignment3/genericMethod/InvalidSubscriptException.java
@@ -8,9 +8,9 @@ package njit.cs602.qiyi.assignment3.genericMethod;
  * @author qiyi
  * @version 2016-4-15
  */
+@SuppressWarnings("serial")
 public class InvalidSubscriptException extends RuntimeException {
 
-    private static final long serialVersionUID = -5115758291750062068L;
     public InvalidSubscriptException(String msg){
         super(msg);
     }
